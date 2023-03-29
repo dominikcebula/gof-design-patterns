cd323c43b27b371eadc376731fcf1e038307636b
Dominik Cebula <dominikcebula@gmail.com>
2023-03-29 20:20:33 +0200

    strategy

--- src/main/java/com/dominikcebula/edu/design/patterns/strategy/shipping/InternationalShippingStrategy.java
+++ src/main/java/com/dominikcebula/edu/design/patterns/strategy/shipping/InternationalShippingStrategy.java
@@ -5,6 +5,7 @@ import org.javamoney.moneta.Money;
 public class InternationalShippingStrategy implements ShippingStrategy {
     private static final double RATE = 0.1;
 
+    @Override
     public Money calculate(Money total) {
         return total.add(total.multiply(RATE));
     }
