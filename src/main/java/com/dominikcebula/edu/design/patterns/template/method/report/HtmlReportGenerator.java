78420aafa5cb12979170f49fad6f601a7ab0d50b
Dominik Cebula <dominikcebula@gmail.com>
2023-03-30 22:12:25 +0200

    template-method

---
+++ src/main/java/com/dominikcebula/edu/design/patterns/template/method/report/HtmlReportGenerator.java
@@ -0,0 +1,18 @@
+package com.dominikcebula.edu.design.patterns.template.method.report;
+
+public class HtmlReportGenerator extends ReportGenerator {
+    @Override
+    void createHeader() {
+        System.out.println("Creating Html Header");
+    }
+
+    @Override
+    void createBody() {
+        System.out.println("Creating Html Body");
+    }
+
+    @Override
+    void createFooter() {
+        System.out.println("Creating Html Footer");
+    }
+}
