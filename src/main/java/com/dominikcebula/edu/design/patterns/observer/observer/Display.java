54f04e6224c2f197267197a7ad86db0b6c10e47c
Dominik Cebula <dominikcebula@gmail.com>
2023-04-02 18:54:33 +0200

    observer

--- src/main/java/com/dominikcebula/edu/design/patterns/observer/observer/Display.java
+++ src/main/java/com/dominikcebula/edu/design/patterns/observer/observer/Display.java
@@ -5,6 +5,9 @@ import com.dominikcebula.edu.design.patterns.observer.data.WeatherData;
 public class Display implements WeatherObserver {
     @Override
     public void update(WeatherData weatherData) {
-        System.out.println("Display: Weather data changed to temperature = " + weatherData.getTemperature() + ", humidity = " + weatherData.getHumidity() + ", pressure = " + weatherData.getPressure());
+        System.out.println("Display: Weather data changed to " +
+                "temperature = " + weatherData.getTemperature()
+                + ", humidity = " + weatherData.getHumidity()
+                + ", pressure = " + weatherData.getPressure());
     }
 }
