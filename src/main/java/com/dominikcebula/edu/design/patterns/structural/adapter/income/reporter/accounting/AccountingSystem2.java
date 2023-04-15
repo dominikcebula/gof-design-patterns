e4abd00cb0156091553e8ff42078e7208195fb7b
Dominik Cebula <dominikcebula@gmail.com>
2023-04-15 22:27:33 +0200

    adapter

--- src/main/java/com/dominikcebula/edu/design/patterns/structural/adapter/income/reporter/accounting/AccountingSystem2.java
+++ src/main/java/com/dominikcebula/edu/design/patterns/structural/adapter/income/reporter/accounting/AccountingSystem2.java
@@ -5,7 +5,7 @@ import com.dominikcebula.edu.design.patterns.structural.adapter.income.reporter.
 public class AccountingSystem2 implements AccountingSystem {
     @Override
     public void reportIncome(IncomeData incomeData) {
-        // Income data reporting details in system 1
+        // Income data reporting details in system 2
         System.out.println("Reporting income data in system 2");
     }
 }
