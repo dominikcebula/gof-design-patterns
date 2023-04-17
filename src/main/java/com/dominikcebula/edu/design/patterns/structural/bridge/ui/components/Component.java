a29ef5ad713f3a2b44650bfd6f373eab8f7907f5
Dominik Cebula <dominikcebula@gmail.com>
2023-04-17 22:07:18 +0200

    bridge

--- src/main/java/com/dominikcebula/edu/design/patterns/structural/bridge/ui/components/Component.java
+++ src/main/java/com/dominikcebula/edu/design/patterns/structural/bridge/ui/components/Component.java
@@ -3,9 +3,9 @@ package com.dominikcebula.edu.design.patterns.structural.bridge.ui.components;
 import com.dominikcebula.edu.design.patterns.structural.bridge.ui.platform.Platform;
 
 abstract class Component {
-    protected final Platform platform;
+    final Platform platform;
 
-    public Component(Platform platform) {
+    Component(Platform platform) {
         this.platform = platform;
     }
 }
